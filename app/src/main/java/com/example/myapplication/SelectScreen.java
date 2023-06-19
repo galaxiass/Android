58b4137968fccc7831abf88a48ef2f37e49b9743
Phaedra <fedra.t03@gmail.com>
2023-06-19 23:45:03 +0300

    made a button functional

--- app/src/main/java/com/example/myapplication/SelectScreen.java
+++ app/src/main/java/com/example/myapplication/SelectScreen.java
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class SelectScreen extends AppCompatActivity {
 
     Button neo_aithma;
-
+    Button activity_financial_movements;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,20 +21,7 @@ public class SelectScreen extends AppCompatActivity {
         //back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //to make the back button functional
-       /* public boolean  onOptionsItemSelected(MenuItem item){
-            switch (item.getItemId()) {
-                case android.R.id.home:
-                    finish();
-                    return true;
-            }
 
-            return super.onOptionsItemSelected(item);
-        }
-
-        public boolean onCreateOptionsMenu(Menu menu) {
-            return true;
-        }*/
 
         //NEO AITHMA functional
         neo_aithma = findViewById(R.id.neo_aithma);
@@ -44,6 +33,28 @@ public class SelectScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        activity_financial_movements = findViewById(R.id.oikon_katastash);
+        activity_financial_movements.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(SelectScreen.this, FinancialMovementsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    //to make the back button functional
+    public boolean  onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
 }
